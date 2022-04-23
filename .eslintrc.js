e5557ae6453b57fe9d9f996e9addaf37b2dc21e3
okakyo <0622okakyo@gmail.com>
2022-04-24 00:47:10 +0900

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
 		browser: true,
 		es2021: true
 	},
-	extends: ['plugin:vue/essential', 'prettier'],
+	extends: [
+		'plugin:vue/essential',
+    '@nuxtjs/eslint-config-typescript',
+    "plugin:tailwindcss/recommended",
+		'prettier'
+	],
 	parserOptions: {
 		ecmaVersion: 'latest',
 		parser: '@typescript-eslint/parser',
 		sourceType: 'module'
 	},
-	plugins: ['vue', '@typescript-eslint'],
+	plugins: ['vue', '@typescript-eslint', 'tailwindcss'],
 	rules: {
 		'vue/multi-word-component-names': 'off'
 	}
