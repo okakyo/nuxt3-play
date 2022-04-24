b2616e5d9305f80dc4efecd02d25bc2c545232a0
okakyo <0622okakyo@gmail.com>
2022-04-24 23:34:53 +0900

    set top page

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="flex justify-between py-5 px-8 bg-gray-100">
+      <div class="">Sample</div>
+      <div>sample</div>
+    </div>
+    <div class="container px-2 mx-auto max-w-md">
+      <slot />
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="">
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div>
+    <div class="py-5 px-3 my-5 rounded-lg border">
+      <h1 class="text-3xl font-bold">Hello world!</h1>
+    </div>
+    <button
+      class="p-3 w-full font-bold text-white bg-main rounded-lg shadow-md"
+    >
+      LOGIN
+    </button>
   </div>
 </template>
