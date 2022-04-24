2eef2f51727faf93dee97ad810db46044c76be6e
okakyo <0622okakyo@gmail.com>
2022-04-24 23:34:29 +0900

    set tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,33 @@ module.exports = {
 		'./pages/**/*.vue'
 	],
 	theme: {
-		extend: {}
+		fontFamily: {
+			sans: ['Graphik', 'sans-serif'],
+			serif: ['Merriweather', 'serif']
+		},
+		borderColor: {
+			DEFAULT: '#cbd5e1'
+		},
+		colors: {
+			transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+			gray: {
+				50: '#f9fafb',
+				100: '#f3f4f6',
+				200: '#e5e7eb',
+				700: '#374151',
+				800: '#1f2937',
+				900: '#1f2937'
+			}
+		},
+		extend: {
+			colors: {
+				base: '#e5e7eb',
+				main: '#059669',
+				accent: '#fb923c'
+			}
+		}
 	},
 	plugins: []
 };
